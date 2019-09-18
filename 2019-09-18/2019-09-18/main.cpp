3ae8c358c68d0006499e0832699ff6e44eec0d6d
tacjoseph <55443251+tacjoseph@users.noreply.github.com>
2019-09-18 08:23:50 -0400

    Add files via upload

---
+++ 2019-09-18/2019-09-18/main.cpp
@@ -0,0 +1,12 @@
+// Name: Joseph Loiseau
+// Intro into Computer Science
+// Period 1
+// Mr.Williams
+// 2019-09-18
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "U CAN TAKE MY GURL BUT NOT MY NODDLES" << std::endl;
+}
