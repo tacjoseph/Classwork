3a33954cb711611f7dae5252ac9e0ba10bccef10
tacjoseph <55443251+tacjoseph@users.noreply.github.com>
2019-09-24 08:38:55 -0400

    Add files via upload

---
+++ 2019-09-24-Classwork/2019-09-24-Classwork/main.cpp
@@ -0,0 +1,19 @@
+// Name: Joseph Loiseau
+// Intro to Computer Science
+// Mr.Williams
+// Period 1
+// 2019-09-19-Classwork
+
+#include <iostream>
+
+int main()
+{
+	int j=  41;
+	int o = 42;
+	int s = 43;
+	int e = 44;
+	int p = 45;
+	int h = 46;
+
+	std::cout << j+o+s+e+p+h << std::endl;
+}
