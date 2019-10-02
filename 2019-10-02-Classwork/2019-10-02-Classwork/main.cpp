770fa2acba4940c1d4d9c5bf22a6c6f806cd8416
tacjoseph <55443251+tacjoseph@users.noreply.github.com>
2019-10-02 08:41:25 -0400

    Add files via upload

---
+++ 2019-10-02-Classwork/2019-10-02-Classwork/main.cpp
@@ -0,0 +1,25 @@
+// Name: Joseph Loiseau
+// Intro into Cs
+// Period 1
+// Mr.Willaims
+// 2019-10-02-Classwork
+#include <iostream>
+
+int main()
+{
+	char b = 'b';
+	char a = 'A';
+	char c = 'c';
+	char d = 'b';
+
+	short life = 1;
+	int succ = 11;
+	unsigned long antony = 600;
+
+	bool isKeef = true;
+	bool isSucc = false;
+
+	float keefin = 10000000;
+	float notkeefin = 1.5;
+	float suc = 123.4;
+}
