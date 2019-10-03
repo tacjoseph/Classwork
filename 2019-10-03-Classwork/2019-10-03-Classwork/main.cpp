b942ac927ad6723c0f1bde92480e38134704948c
tacjoseph <55443251+tacjoseph@users.noreply.github.com>
2019-10-03 08:41:08 -0400

    Add files via upload

---
+++ 2019-10-03-Classwork/2019-10-03-Classwork/main.cpp
@@ -0,0 +1,17 @@
+// Name: Joseph Loiseau
+// Intro to CS
+// Period 1
+// Mr.Williams
+// 2019-10-03-Classwork
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Enter your favorite number:";
+	int favoriteNumber;
+	std::cin >> favoriteNumber;
+	std::cout << "Cool," << favoriteNumber << "is my favorite number tool!";
+
+}
